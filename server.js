06cf40cc183e0e311a9730ef20a377fc5991e6fc
adsterra4 <150966871+adsterra4@users.noreply.github.com>
2023-11-24 17:56:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@
     <div id="paypal-button-container"></div>
     <p id="result-message"></p>
     <!-- Replace the "test" client-id value with your client-id -->
-    <script src="https://www.paypal.com/sdk/js?client-id=test&components=buttons&enable-funding=paylater,venmo,card" data-sdk-integration-source="integrationbuilder_sc"></script>
+    <script src="https://www.paypal.com/sdk/js?client-id=AQDuwzK3B9zqT4aXHS7kHvTK2A2EQeVtrPaRtaW9SZ34izNhAQNymZ0-S6msz4X2fXwE9usw8Gjiw0xo&components=buttons&enable-funding=paylater,venmo,card" data-sdk-integration-source="integrationbuilder_sc"></script>
     <script src="app.js"></script>
   </body>
 </html>
